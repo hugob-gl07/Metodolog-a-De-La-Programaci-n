0cd8ccb729755ebed34756b47c8e9ae2f10d140f
mrodriguezg16 <160848580+mrodriguezg16@users.noreply.github.com>
2026-03-17 19:10:09 +0100

    Update Animal.java

--- MP-EL1-D/src/Ejercicios/Animal/Animal.java
+++ MP-EL1-D/src/Ejercicios/Animal/Animal.java
@@ -1,11 +1,11 @@
 package Ejercicios.Animal;
-
+//Clase abstracta Animal
 public abstract class Animal {
     protected String name;
-
+//Constructor sencillo para inicializar la clase.
     public Animal(String name) {
         this.name = name;
     }
-
+//Método abstracto que luego sera implementado de forma distinta entre sus clases hijas.
     public abstract void greets();
 }
