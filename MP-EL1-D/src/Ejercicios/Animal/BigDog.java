7b9447320d6b46c1657be0875a20309800f5c351
mrodriguezg16 <160848580+mrodriguezg16@users.noreply.github.com>
2026-03-17 19:19:04 +0100

    Update BigDog.java

--- MP-EL1-D/src/Ejercicios/Animal/BigDog.java
+++ MP-EL1-D/src/Ejercicios/Animal/BigDog.java
@@ -1,20 +1,21 @@
 package Ejercicios.Animal;
-
+//BigDog extiende a la clase Dog (clase hija).
 public class BigDog extends Dog {
+    //Constructor sencillo que implementa el atruuto de la clase abstracta Animal.
     public BigDog(String name) {
         super(name);
     }
-
+//Método greets propio de BigDog
     @Override
     public void greets() {
         System.out.println("Wooow");
     }
-
+//Al igual que la clase Dog, en caso de array con la clase Dog (que haya un BigDog y más de un Dog), tiene un método correspondiente.
     @Override
     public void greets(Dog another) {
         System.out.println("Woooooow");
     }
-
+//Al igual que la clase Dog, en caso de array, tiene un método correspondiente:
     public void greets(BigDog another) {
         System.out.println("Wooooooooow");
     }
