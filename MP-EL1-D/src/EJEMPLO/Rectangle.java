fb8af06d3d454f509a2e8d5b03649eca9c17da1a
mrodriguezg16 <160848580+mrodriguezg16@users.noreply.github.com>
2026-03-17 18:57:44 +0100

    Update Rectangle.java

--- MP-EL1-D/src/EJEMPLO/Rectangle.java
+++ MP-EL1-D/src/EJEMPLO/Rectangle.java
@@ -1,25 +1,26 @@
 package EJEMPLO;
-
+//Clase rectangulo, extiende a la clase abstracta Shape
 public class Rectangle extends Shape {
     protected double width = 1.0;
     protected double length = 1.0;
-
-    public Rectangle() {}
-    public Rectangle(double width, double length) {
+//Constructores:
+    public Rectangle() {}//Constructor vacío
+    public Rectangle(double width, double length) {//Constructor simple: implementa los elementos propios de rectangulo.
         this.width = width;
         this.length = length;
     }
-    public Rectangle(double width, double length, String color, boolean filled) {
+    public Rectangle(double width, double length, String color, boolean filled) {//Constructor extendido: añade los elementos de la clase abstracta
+                                                                                 //y los elementos de la clase hija (width y length)
         super(color, filled);
         this.width = width;
         this.length = length;
     }
-
+//Getters y Setters
     public double getWidth() { return width; }
     public void setWidth(double width) { this.width = width; }
     public double getLength() { return length; }
     public void setLength(double length) { this.length = length; }
-
+//
     @Override
     public double getArea() { return width * length; }
 
