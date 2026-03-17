c53b50484947f587ff2e7ef1eebc332e41b97b30
mrodriguezg16 <160848580+mrodriguezg16@users.noreply.github.com>
2026-03-17 19:13:13 +0100

    Update Dog.java

--- MP-EL1-D/src/Ejercicios/Animal/Dog.java
+++ MP-EL1-D/src/Ejercicios/Animal/Dog.java
@@ -1,15 +1,16 @@
 package Ejercicios.Animal;
-
+//Extiende a la clase Animal
 public class Dog extends Animal {
+    //Constructor sencillo extendiendo el atributo nombre de la clase abstracta.
     public Dog(String name) {
         super(name);
     }
-
+//Método greets propio de la clase perro
     @Override
     public void greets() {
         System.out.println("Woof");
     }
-
+//Método greets en el caso de que tengamos un array de la clase perro.
     public void greets(Dog another) {
         System.out.println("Woooof");
     }
