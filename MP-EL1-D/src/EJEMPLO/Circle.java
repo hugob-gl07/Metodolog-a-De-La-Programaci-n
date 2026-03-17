98a0d83365f0a0301d6c353dd5512a505cbce908
mrodriguezg16 <160848580+mrodriguezg16@users.noreply.github.com>
2026-03-17 18:54:21 +0100

    Update Circle.java

--- MP-EL1-D/src/EJEMPLO/Circle.java
+++ MP-EL1-D/src/EJEMPLO/Circle.java
@@ -1,24 +1,24 @@
 package EJEMPLO;
-
+//Clase hija de la clase abstracta Shape
 public class Circle extends Shape {
     protected double radius = 1.0;
-
-    public Circle() {}
-    public Circle(double radius) { this.radius = radius; }
-    public Circle(double radius, String color, boolean filled) {
+//Constructores:
+    public Circle() {}//Constructor vacío
+    public Circle(double radius) { this.radius = radius; } //Constructor simple (solo con el elemento propio de circulo
+    public Circle(double radius, String color, boolean filled) {//Constructor extendido, agrega los datos protegidos de la clase Shape
         super(color, filled);
         this.radius = radius;
     }
-
+//Getter y Setter
     public double getRadius() { return radius; }
     public void setRadius(double radius) { this.radius = radius; }
-
+//Método abstracto adaptado a la clase circulo.
     @Override
     public double getArea() { return Math.PI * radius * radius; }
-
+//Método abstracto adaptado a la clase circulo.
     @Override
     public double getPerimeter() { return 2 * Math.PI * radius; }
-
+//Extiende el ToString de la clase abstracta, devuelve por pantalla el radio, el color y el relleno.
     @Override
     public String toString() {
         return "Circle[" + super.toString() + ", radius=" + radius + "]";
