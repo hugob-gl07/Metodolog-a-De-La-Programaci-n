1c6335cc76ac6f416f2ada07378480c03f4e349a
mrodriguezg16 <160848580+mrodriguezg16@users.noreply.github.com>
2026-03-17 19:21:45 +0100

    Update GeometricObject.java

--- MP-EL1-D/src/Ejercicios/Resizable_Y_GeometricObject/GeometricObject.java
+++ MP-EL1-D/src/Ejercicios/Resizable_Y_GeometricObject/GeometricObject.java
@@ -1,5 +1,5 @@
 package Ejercicios.Resizable_Y_GeometricObject;
-
+//Interfaz Geometric object (Todos los objetos que usen GeometricObject contendrán todos los métodos aqui presentes):
 public interface GeometricObject {
     double getPerimeter();
     double getArea();
