aa800f3d5dbdd8b33331c50616a789a5453e8bda
mrodriguezg16 <160848580+mrodriguezg16@users.noreply.github.com>
2026-03-17 19:15:18 +0100

    Update Cat.java

--- MP-EL1-D/src/Ejercicios/Animal/Cat.java
+++ MP-EL1-D/src/Ejercicios/Animal/Cat.java
@@ -1,10 +1,11 @@
 package Ejercicios.Animal;
-
+//Clase Gato, extiende a la clase abstracta Animal.
 public class Cat extends Animal {
+    //Constructor sencillo que emplea el atributo de la clase abstracta (inicializa la clase Gato).
     public Cat(String name) {
         super(name);
     }
-
+//Método greets propio de la clase Gato.
     @Override
     public void greets() {
         System.out.println("Meow");
