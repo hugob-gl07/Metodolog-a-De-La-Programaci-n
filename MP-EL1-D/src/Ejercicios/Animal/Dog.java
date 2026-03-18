38c618686ca58dfa90b51da72adde6213b038d64
mrodriguezg16 <160848580+mrodriguezg16@users.noreply.github.com>
2026-03-18 19:24:02 +0100

    Update Dog.java

--- MP-EL1-D/src/Ejercicios/Animal/Dog.java
+++ MP-EL1-D/src/Ejercicios/Animal/Dog.java
@@ -1,17 +1,20 @@
 package Ejercicios.Animal;
-//Extiende a la clase Animal
+
+// Dog extiende a Animal e introduce la interacción entre objetos del mismo tipo.
 public class Dog extends Animal {
-    //Constructor sencillo extendiendo el atributo nombre de la clase abstracta.
     public Dog(String name) {
         super(name);
     }
-//Método greets propio de la clase perro
+
+    // Sobrescritura del método básico de Animal.
     @Override
     public void greets() {
         System.out.println("Woof");
     }
-//Método greets en el caso de que tengamos un array de la clase perro.
+
+    // SOBRECARGA: Nuevo método que permite interactuar con otro perro.
+    // No usa @Override porque Animal no conocía este método con parámetros.
     public void greets(Dog another) {
-        System.out.println("Woooof");
+        System.out.println("Woooof");//Devuelve por pantalla el resultado del método greets propio cuando hay más de un perro (Sobrecarga).
     }
 }
