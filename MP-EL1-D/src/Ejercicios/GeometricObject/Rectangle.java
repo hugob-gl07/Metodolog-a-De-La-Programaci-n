afe0fb1d06ac355f9ce005be48c25eb13ce33b9b
mrodriguezg16 <160848580+mrodriguezg16@users.noreply.github.com>
2026-03-18 19:09:48 +0100

    Update Rectangle.java

--- MP-EL1-D/src/Ejercicios/GeometricObject/Rectangle.java
+++ MP-EL1-D/src/Ejercicios/GeometricObject/Rectangle.java
@@ -1,20 +1,35 @@
 package Ejercicios.GeometricObject;
 
+// Al igual que Circle, Rectangle implementa la misma interfaz, 
+// permitiendo que ambos sean tratados de forma polimórfica.
 public class Rectangle implements GeometricObject {
-    private double width;
-    private double length;
+    private double width;//Atributo 1
+    private double length;//atributo 2
 
+    // Constructor para definir las dos dimensiones necesarias.
     public Rectangle(double width, double length) {
         this.width = width;
         this.length = length;
-    } // [cite: 143]
+    } 
 
+    // Cálculo de superficie adaptado a la geometría rectangular, emplea la operación "width*length".
+    //Devuelve por pantalla el área del rectangulo en forma del tipo básico double
     @Override
-    public double getArea() { return width * length; }
+    public double getArea() { 
+        return width * length; 
+    }
 
+    // Cálculo del contorno sumando los cuatro lados, es decir, usando la formula "2*(width + length)".
+     //Devuelve por pantalla el perímetro del rectangulo en forma del tipo básico double.
     @Override
-    public double getPerimeter() { return 2 * (width + length); }
+    public double getPerimeter() { 
+        return 2 * (width + length); 
+    }
 
+    //Devuelve por pantalla los atgribbutos de dicho rectangulo (width y length).
+    //Dicha información se imprime por pantalla en forma de tipo básico string.
     @Override
-    public String toString() { return "Rectangle[width=" + width + ", length=" + length + "]"; }
+    public String toString() { 
+        return "Rectangle[width=" + width + ", length=" + length + "]"; 
+    }
 }
