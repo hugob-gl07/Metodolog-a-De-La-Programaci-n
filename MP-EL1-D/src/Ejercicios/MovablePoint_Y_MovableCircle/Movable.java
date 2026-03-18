a8b76a1011eec28a4b35956804f0663ff9d00388
mrodriguezg16 <160848580+mrodriguezg16@users.noreply.github.com>
2026-03-18 18:49:06 +0100

    Update Movable.java

--- MP-EL1-D/src/Ejercicios/MovablePoint_Y_MovableCircle/Movable.java
+++ MP-EL1-D/src/Ejercicios/MovablePoint_Y_MovableCircle/Movable.java
@@ -1,5 +1,8 @@
 package Ejercicios.MovablePoint_Y_MovableCircle;
 
+// La interfaz Movable define un "contrato de movimiento". 
+// Cualquier clase que la implemente garantiza que tendrá estos cuatro métodos.
+// Esto permite que el sistema trate a puntos y círculos de la misma forma (Polimorfismo).
 public interface Movable {
     void moveUp();
     void moveDown();
