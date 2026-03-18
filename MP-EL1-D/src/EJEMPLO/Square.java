bdb23271955deac5dd2064ae137b54b523f448f2
mrodriguezg16 <160848580+mrodriguezg16@users.noreply.github.com>
2026-03-18 18:42:00 +0100

    Update Square.java

--- MP-EL1-D/src/EJEMPLO/Square.java
+++ MP-EL1-D/src/EJEMPLO/Square.java
@@ -15,12 +15,14 @@ public class Square extends Rectangle {
         setLength(side);
     }
 //De este modo al ejecutar los setters del rectangulo, establecemos que length y widht son lo mismo (tienen el mismo valor: el lado).
+    //cabe destacar que ambos setters devuelven el tipo básico double.
     @Override
     public void setWidth(double side) { super.setWidth(side); super.setLength(side); }
     @Override
     public void setLength(double side) { super.setLength(side); super.setWidth(side); }
 //Extensión del ToString del rect<ngulo, con la diferencia de que largo y ancho tendran el mismo valor (lado).
-//Cabe destacar que se devuelve por pantalla el color y el relleno de la clase abstracta Shape.
+//Cabe destacar que se devuelve por pantalla el color y el relleno de la clase abstracta Shape gracias al ToString, que sobrecarga el método
+    //Devyuelve por pantalla el tipo básico string.
     @Override
     public String toString() {
         return "Square[" + super.toString() + "]";
