a823640393c220cfe5eb20cceb5637bd8ec54649
mrodriguezg16 <160848580+mrodriguezg16@users.noreply.github.com>
2026-03-18 18:56:52 +0100

    Update TestMovable.java

--- MP-EL1-D/src/Ejercicios/MovablePoint_Y_MovableCircle/TestMovable.java
+++ MP-EL1-D/src/Ejercicios/MovablePoint_Y_MovableCircle/TestMovable.java
@@ -2,11 +2,14 @@ package Ejercicios.MovablePoint_Y_MovableCircle;
 
 public class TestMovable {
     public static void main(String[] args) {
-        Movable m1 = new MovablePoint(5, 5, 10, 10); // Polimorfismo
+        // Ejemplo de Polimorfismo: Una variable de tipo interfaz (Movable)
+        // puede almacenar objetos de cualquier clase que la implemente.
+        Movable m1 = new MovablePoint(5, 5, 10, 10); 
         m1.moveUp();
         System.out.println("Punto: " + m1);
 
-        Movable m2 = new MovableCircle(2, 1, 2, 2, 20); // Polimorfismo
+        // Aquí m2, siendo del mismo tipo Movable, ahora se comporta como un círculo.
+        Movable m2 = new MovableCircle(2, 1, 2, 2, 20); 
         m2.moveRight();
         System.out.println("Círculo: " + m2);
     }
