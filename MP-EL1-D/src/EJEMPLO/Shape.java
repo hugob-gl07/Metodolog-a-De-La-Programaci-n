8a9ede6587dd38165c2ca9f65e471c445546f29d
mrodriguezg16 <160848580+mrodriguezg16@users.noreply.github.com>
2026-03-18 18:39:17 +0100

    Update Shape.java

--- MP-EL1-D/src/EJEMPLO/Shape.java
+++ MP-EL1-D/src/EJEMPLO/Shape.java
@@ -11,14 +11,16 @@ public abstract class Shape {
         this.filled = filled;
     }
 //Métodos que componen nuestra clase abstracta:
-    public String getColor() { return color; }
-    public void setColor(String color) { this.color = color; }
-    public boolean isFilled() { return filled; }
-    public void setFilled(boolean filled) { this.filled = filled; }
-//Estos métodos seran implementados de forma distinta en las clases hijas.
+    public String getColor() { return color; }//Getter
+    public void setColor(String color) { this.color = color; }//Setter
+    public boolean isFilled() { return filled; }//Método que trabaja con el tipo básico bool (True/False)
+    public void setFilled(boolean filled) { this.filled = filled; }//Setter que establece una relación con el método isFilled
+//Estos métodos seran implementados de forma distinta en las clases hijas, al estar en la clase abstracta, lo especificamos
+    //escribiendo abstract, también indicamos en el tipo básico de datos en el que vamos a trabajar.
     public abstract double getArea();
     public abstract double getPerimeter();
 //ToString para devolver por pantalla el color y el relleno de la clase Shape.
+    //Destacamos que devuelve el tipo básico string por pantalla.
     @Override
     public String toString() {
         return "Shape[color=" + color + ", filled=" + filled + "]";
