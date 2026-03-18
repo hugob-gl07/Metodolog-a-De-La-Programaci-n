823a75cf27bd93c717b8c9e410df12e10beb9095
mrodriguezg16 <160848580+mrodriguezg16@users.noreply.github.com>
2026-03-18 19:03:58 +0100

    Update GeometricObject.java

--- MP-EL1-D/src/Ejercicios/GeometricObject/GeometricObject.java
+++ MP-EL1-D/src/Ejercicios/GeometricObject/GeometricObject.java
@@ -1,6 +1,11 @@
 package Ejercicios.GeometricObject;
 
+// Una interfaz define un "contrato" de comportamiento.
+// No almacena datos, solo declara qué métodos deben implementar las clases hijas.
 public interface GeometricObject {
-    double getArea(); //
-    double getPerimeter(); //
+    // Método abstracto para calcular la superficie.
+    double getArea(); 
+    
+    // Método abstracto para calcular el contorno de la figura.
+    double getPerimeter(); 
 }
